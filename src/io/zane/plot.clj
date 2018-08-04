d3dd0c7c6c71e7fe72f69074eb4e689ac9a7ebdf
Zane Shelby <zane@users.noreply.github.com>
2018-08-04 01:43:26 -0400

    Extract out examples.clj

--- src/io/zane/plot.clj
+++ src/io/zane/plot.clj
@@ -1,6 +1,5 @@
 (ns io.zane.plot
-  (:require [incanter.stats :as stats]
-            [io.zane.plot.axes :as axes]
+  (:require [io.zane.plot.axes :as axes]
             [io.zane.plot.border :as border]
             [io.zane.plot.canvas :as canvas]
             [io.zane.plot.matrix :as matrix]
@@ -8,7 +7,7 @@
             [io.zane.plot.compress.heatmap :as heatmap]
             [io.zane.plot.compress.braille :as braille]))
 
-(defn plot
+(defn draw
   "Generates a plot from the provided data."
   ([pts compress]
    (plot pts compress {}))
@@ -31,48 +30,3 @@
          (axes/draw x-min x-max y-min y-max)
          (matrix/string)
          (print)))))
-
-(defn test-sin-cos
-  []
-  (plot (mapcat (fn [x]
-                  [[x (Math/cos x)]
-                   [x (Math/sin x)]])
-                (range -2 7 1/100))
-        braille/compress))
-
-(defn test-pow
-  []
-  (plot (for [x (range 0 10 1/25)]
-          [x (float (* x x))])
-        braille/compress))
-
-(defn test-scatter
-  []
-  (let [size 500]
-    (plot (map vector
-               (stats/sample-normal size)
-               (stats/sample-normal size))
-          braille/compress)))
-
-(defn test-heatmap
-  []
-  (let [size 1000]
-    (plot (map vector
-               (stats/sample-normal size)
-               (stats/sample-normal size))
-          heatmap/compress)))
-
-(defn test-corners
-  []
-  (plot (for [x [0 1]
-              y [0 1]]
-          [x y])
-        braille/compress
-        {:width 1
-         :height 1}))
-
-#_(test-sin-cos)
-#_(test-pow)
-#_(test-scatter)
-#_(test-heatmap)
-#_(test-corners)
